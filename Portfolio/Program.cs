8cfa6bbb330f51d7bc49bd138b9bab0be0451948
Angelo Marangon <angelo.marangon1@icloud.com>
2025-04-25 04:09:42 +0200

    Changed net 8

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -43,13 +43,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
+
 
 app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
